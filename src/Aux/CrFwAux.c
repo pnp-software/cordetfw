d63eec2996f3d08361b4e3db7e10f1ce40aee324
Vaclav Cechticky <vaclav.cechticky@pnp-software.com>
2015-11-20 16:43:47 +0100

    Initial commit

---
+++ src/Aux/CrFwAux.c
@@ -0,0 +1,261 @@
+/**
+ * @file
+ * @ingroup auxGroup
+ * Implementation of Auxiliary Module.
+ *
+ * @author Vaclav Cechticky <vaclav.cechticky@pnp-software.com>
+ * @author Alessandro Pasetti <pasetti@pnp-software.com>
+ * @copyright P&P Software GmbH, 2013, All Rights Reserved
+ *
+ * This file is part of CORDET Framework.
+ *
+ * CORDET Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CORDET Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CORDET Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For information on alternative licensing, please contact P&P Software GmbH.
+ */
+
+#include <stdlib.h>
+/* Include framework files */
+#include "CrFwConstants.h"
+#include "UtilityFunctions/CrFwUtilityFunctions.h"
+#include "CrFwAux.h"
+#include "OutCmp/CrFwOutCmp.h"
+/* Include configuration files */
+#include "CrFwOutRegistryUserPar.h"
+#include "CrFwOutFactoryUserPar.h"
+#include "CrFwInFactoryUserPar.h"
+#include "CrFwInStreamUserPar.h"
+#include "CrFwOutStreamUserPar.h"
+#include "CrFwOutManagerUserPar.h"
+
+/** Array of OutComponent descriptors. */
+static CrFwOutCmpKindDesc_t outCmpKindDesc[CR_FW_OUTCMP_NKINDS] = CR_FW_OUTCMP_INIT_KIND_DESC;
+
+/** Array of InCommand descriptors. */
+static CrFwInCmdKindDesc_t inCmdKindDesc[CR_FW_INCMD_NKINDS] = CR_FW_INCMD_INIT_KIND_DESC;
+
+/** Array of InReport service descriptors. */
+static CrFwInRepKindDesc_t inRepKindDesc[CR_FW_INREP_NKINDS] = CR_FW_INREP_INIT_KIND_DESC;
+
+/** Array of service descriptors. */
+static CrFwServDesc_t servDesc[CR_FW_OUTREGISTRY_NSERV] = CR_FW_OUTREGISTRY_INIT_SERV_DESC;
+
+/** The sizes of the packet queues in the InStream components. */
+static CrFwCounterU1_t inStreamPcktQueueSize[CR_FW_NOF_INSTREAM] = CR_FW_INSTREAM_PQSIZE;
+
+/** The sizes of the packet queues in the OutStream components. */
+static CrFwCounterU1_t outStreamPcktQueueSize[CR_FW_NOF_OUTSTREAM] = CR_FW_OUTSTREAM_PQSIZE;
+
+/** The sizes of the POCL in the OutManager components. */
+static CrFwCounterU1_t outManagerPoclSize[CR_FW_NOF_OUTMANAGER] = CR_FW_OUTMANAGER_POCLSIZE;
+
+/* -------------------------------------------------------------------------- */
+CrFwConfigCheckOutcome_t CrFwAuxConfigCheck() {
+
+	if (CrFwAuxOutRegistryConfigCheck() == 0)
+		return crOutRegistryConfigParInconsistent;
+
+	if (CrFwAuxOutFactoryConfigCheck() == 0)
+		return crOutFactoryConfigParInconsistent;
+
+	if (CrFwAuxInFactoryInCmdConfigCheck() == 0)
+		return crInFactoryInCmdConfigParInconsistent;
+
+	if (CrFwAuxInFactoryInRepConfigCheck() == 0)
+		return crInFactoryInRepConfigParInconsistent;
+
+	return crConsistencyCheckSuccess;
+}
+
+/* -------------------------------------------------------------------------- */
+CrFwBool_t CrFwAuxOutRegistryConfigCheck() {
+	CrFwCmdRepIndex_t i;
+	CrFwCmdRepKindIndex_t j;
+	CrFwCounterU1_t k;
+	CrFwServType_t servType;
+	CrFwServSubType_t servSubType;
+	CrFwBool_t found;
+
+	if (CR_FW_OUTREGISTRY_N < 1)
+		return 0;
+
+	for (i=0; i<(CR_FW_OUTREGISTRY_NSERV-1); i++) {
+		if (servDesc[i].servType > servDesc[i+1].servType)
+			return 0;
+		if (servDesc[i].servType == servDesc[i+1].servType)
+			if (servDesc[i].servSubType > servDesc[i+1].servSubType)
+				return 0;
+	}
+
+	for (i=0; i<(CR_FW_OUTREGISTRY_NSERV-1); i++) {
+		servType = servDesc[i].servType;
+		servSubType = servDesc[i].servSubType;
+		found = 0;
+		for (j=0; j<CR_FW_OUTCMP_NKINDS; j++) {
+			if (outCmpKindDesc[j].servType == servType)
+				if (outCmpKindDesc[j].servSubType == servSubType) {
+					found = 1;
+					break;
+				}
+			if (outCmpKindDesc[j].servType > servType)
+				break;
+		}
+		if (found == 0)
+			return 0;
+	}
+
+	for (k=0; k<CR_FW_NOF_INSTREAM; k++)
+		if (inStreamPcktQueueSize[k]<1)
+			return 0;
+
+	for (k=0; k<CR_FW_NOF_OUTSTREAM; k++)
+		if (outStreamPcktQueueSize[k]<1)
+			return 0;
+
+	for (k=0; k<CR_FW_NOF_OUTMANAGER; k++)
+		if (outManagerPoclSize[k]<1)
+			return 0;
+
+	return 1;
+}
+
+/* -------------------------------------------------------------------------- */
+CrFwBool_t CrFwAuxOutFactoryConfigCheck() {
+	CrFwCmdRepIndex_t j;
+	CrFwCmdRepKindIndex_t i;
+	CrFwServType_t servType;
+	CrFwServSubType_t servSubType;
+	CrFwDiscriminant_t disc;
+	CrFwBool_t found;
+
+	if (CR_FW_OUTFACTORY_MAX_NOF_OUTCMP < 1)
+		return 0;
+
+	if (CR_FW_OUTCMP_NKINDS < 1)
+		return 0;
+
+	for (i=0; i<(CR_FW_OUTCMP_NKINDS-1); i++) {
+		if (outCmpKindDesc[i].servType > outCmpKindDesc[i+1].servType)
+			return 0;
+
+		if (outCmpKindDesc[i].servType == outCmpKindDesc[i+1].servType)
+			if (outCmpKindDesc[i].servSubType > outCmpKindDesc[i+1].servSubType)
+				return 0;
+
+		if (outCmpKindDesc[i].servType == outCmpKindDesc[i+1].servType)
+			if (outCmpKindDesc[i].servSubType == outCmpKindDesc[i+1].servSubType)
+				if (outCmpKindDesc[i].discriminant > outCmpKindDesc[i+1].discriminant)
+					return 0;
+
+		if (outCmpKindDesc[i].pcktLength < 1)
+			return 0;
+	}
+
+	for (i=0; i<CR_FW_OUTCMP_NKINDS; i++) {
+		servType = outCmpKindDesc[i].servType;
+		servSubType = outCmpKindDesc[i].servSubType;
+		disc = outCmpKindDesc[i].discriminant;
+		found = 0;
+		for (j=0; j<CR_FW_OUTREGISTRY_NSERV; j++) {
+			if (servDesc[j].servType == servType)
+				if (servDesc[j].servSubType == servSubType)
+					if (servDesc[j].maxDiscriminant >= disc) {
+						found = 1;
+						break;
+					}
+			if (servDesc[j].servType > servType)
+				break;
+		}
+		if (found == 0)
+			return 0;
+	}
+
+	return 1;
+}
+
+/* -------------------------------------------------------------------------- */
+CrFwBool_t CrFwAuxInFactoryInCmdConfigCheck() {
+	CrFwCmdRepKindIndex_t i;
+
+	if (CR_FW_INFACTORY_MAX_NOF_INCMD < 1)
+		return 0;
+
+	if (CR_FW_OUTCMP_NKINDS < 1)
+		return 0;
+
+	for (i=0; i<(CR_FW_INCMD_NKINDS-1); i++) {
+		if (inCmdKindDesc[i].servType > inCmdKindDesc[i+1].servType)
+			return 0;
+
+		if (inCmdKindDesc[i].servType == inCmdKindDesc[i+1].servType)
+			if (inCmdKindDesc[i].servSubType > inCmdKindDesc[i+1].servSubType)
+				return 0;
+
+		if (inCmdKindDesc[i].servType == inCmdKindDesc[i+1].servType)
+			if (inCmdKindDesc[i].servSubType == inCmdKindDesc[i+1].servSubType)
+				if (inCmdKindDesc[i].discriminant > inCmdKindDesc[i+1].discriminant)
+					return 0;
+	}
+
+	for (i=0; i<CR_FW_INCMD_NKINDS; i++) {
+		if (inCmdKindDesc[i].servType > CR_FW_MAX_SERV_TYPE)
+			return 0;
+
+		if (inCmdKindDesc[i].servSubType > CR_FW_MAX_SERV_SUBTYPE)
+			return 0;
+
+		if (inCmdKindDesc[i].discriminant > CR_FW_MAX_DISCRIMINANT)
+			return 0;
+	}
+
+	return 1;
+}
+
+/* -------------------------------------------------------------------------- */
+CrFwBool_t CrFwAuxInFactoryInRepConfigCheck() {
+	CrFwCmdRepKindIndex_t i;
+
+	if (CR_FW_INFACTORY_MAX_NOF_INREP < 1)
+		return 0;
+
+	if (CR_FW_INREP_NKINDS < 1)
+		return 0;
+
+	for (i=0; i<(CR_FW_INREP_NKINDS-1); i++) {
+		if (inRepKindDesc[i].servType > inRepKindDesc[i+1].servType)
+			return 0;
+
+		if (inRepKindDesc[i].servType == inRepKindDesc[i+1].servType)
+			if (inRepKindDesc[i].servSubType > inRepKindDesc[i+1].servSubType)
+				return 0;
+
+		if (inRepKindDesc[i].servType == inRepKindDesc[i+1].servType)
+			if (inRepKindDesc[i].servSubType == inRepKindDesc[i+1].servSubType)
+				if (inRepKindDesc[i].discriminant > inRepKindDesc[i+1].discriminant)
+					return 0;
+	}
+
+	for (i=0; i<CR_FW_INREP_NKINDS; i++) {
+		if (inRepKindDesc[i].servType > CR_FW_MAX_SERV_TYPE)
+			return 0;
+
+		if (inRepKindDesc[i].servSubType > CR_FW_MAX_SERV_SUBTYPE)
+			return 0;
+
+		if (inRepKindDesc[i].discriminant > CR_FW_MAX_DISCRIMINANT)
+			return 0;
+	}
+
+	return 1;
+}
---
+++ src/Aux/CrFwAux.h
@@ -0,0 +1,164 @@
+/**
+ * @file
+ * @ingroup auxGroup
+ * Auxiliary module to check the static part of an application's configuration.
+ * The configuration of an application instantiated from the CORDET Framework is,
+ * for the most part, defined statically in files with names like:
+ * <code>CrFwXxxUserPar</code> where "Xxx" is the name of the framework component
+ * to which the configuration information applies.
+ * This module defines a set of functions which check the consistency of the
+ * configuration information in these configuration files.
+ * A function is defined for each configuration file and, additionally,
+ * function <code>::CrFwAuxConfigCheck</code> checks all the configuration files.
+ *
+ * <b>Mode of Use of the Aux Module</b>
+ *
+ * Applications would normally use the functions in this module during the
+ * application development phase to verify the correctness of the information
+ * in the configuration files.
+ * Once this correctness has been confirmed, the configuration checks can be
+ * omitted.
+ * It is therefore not expected that this module will be included in the final
+ * executable for an application.
+ *
+ * @author Vaclav Cechticky <vaclav.cechticky@pnp-software.com>
+ * @author Alessandro Pasetti <pasetti@pnp-software.com>
+ * @copyright P&P Software GmbH, 2013, All Rights Reserved
+ *
+ * This file is part of CORDET Framework.
+ *
+ * CORDET Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CORDET Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CORDET Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For information on alternative licensing, please contact P&P Software GmbH.
+ */
+
+#ifndef CRFW_AUX_H_
+#define CRFW_AUX_H_
+
+#include "CrFwConstants.h"
+
+/** Type for the outcome of the consistency check on the configuration parameters */
+typedef enum {
+	/** All configuration consistency checks have been passed  */
+	crConsistencyCheckSuccess = 1,
+	/** OutRegistry configuration parameters are inconsistent (see <code>CrFwAuxOutRegistryConfigCheck.h</code>) */
+	crOutRegistryConfigParInconsistent = 2,
+	/** OutFactory configuration parameters are inconsistent (see <code>CrFwAuxOutFactoryConfigCheck.h</code>) */
+	crOutFactoryConfigParInconsistent = 3,
+	/** InFactory configuration parameters for InCommands are inconsistent (see <code>CrFwAuxInFactoryInCmdConfigCheck.h</code>) */
+	crInFactoryInCmdConfigParInconsistent = 4,
+	/** InFactory configuration parameters for InReports are inconsistent (see <code>CrFwAuxInFactoryInRepConfigCheck.h</code>) */
+	crInFactoryInRepConfigParInconsistent = 5
+} CrFwConfigCheckOutcome_t;
+
+/**
+ * Check the configuration of an application.
+ * This function calls all the configuration check functions and verifies that
+ * they all return true (to signify: "no configuration error detected").
+ * The function returns false as soon as it encounters a configuration check function
+ * which returns false.
+ * @return a value from <code>CrFwConfigCheckOutcome_t</code> indicating which configuration
+ * check has failed.
+ */
+CrFwConfigCheckOutcome_t CrFwAuxConfigCheck();
+
+/**
+ * Check the configuration of the OutRegistry component.
+ * The following checks are performed on the configuration data in
+ * <code>CrFwOutRegistryUserPar.h</code>:
+ * - CR_FW_OUTREGISTRY_N is greater than zero
+ * - The service types are listed in increasing order in the service descriptor
+ *   initializer (<code>CR_FW_OUTREGISTRY_INIT_SERV_DESC</code>)
+ * - The service sub-types within a type are listed in increasing order in the service
+ *   descriptor initializer (<code>CR_FW_OUTREGISTRY_INIT_SERV_DESC</code>)
+ * - The service types, sub-types and discriminant values defined in the
+ *   <code>#CR_FW_OUTREGISTRY_INIT_SERV_DESC</code> initializer are consistent with
+ *   service type, sub-types and discriminant values defined in the
+ *   <code>#CR_FW_OUTCMP_INIT_KIND_DESC</code>.
+ * - The size of the InStream packet queue is greater than zero.
+ * - The size of the OutStream packet queue is greater than zero.
+ * - The size of the Pending OutComponent List (POCL) is greater than zero.
+ * .
+ * @return true if no errors are detected in the configuration data;
+ * false otherwise.
+ */
+CrFwBool_t CrFwAuxOutRegistryConfigCheck();
+
+/**
+ * Check the configuration of the OutFactory component.
+ * The following checks are performed on the configuration data in
+ * <code>CrFwOutFactoryUserPar.h</code>:
+ * - CR_FW_OUTFACTORY_MAX_NOF_OUTCMP is greater than zero
+ * - CR_FW_OUTCMP_NKINDS is greater than zero
+ * - The service types are listed in increasing order in the service descriptor
+ *   initializer (<code>CR_FW_OUTCMP_INIT_KIND_DESC</code>)
+ * - The service sub-types within a type are listed in increasing order in the service
+ *   descriptor initializer (<code>CR_FW_OUTCMP_INIT_KIND_DESC</code>)
+ * - The discriminant values within a type/sub-type are listed in increasing order
+ *   in the service descriptor initializer (<code>CR_FW_OUTCMP_INIT_KIND_DESC</code>)
+ * - The service types, sub-types and discriminant values defined in the
+ *   <code>#CR_FW_OUTCMP_INIT_KIND_DESC</code> initializer are consistent with
+ *   service type, sub-types and discriminant values defined in the
+ *   <code>#CR_FW_OUTREGISTRY_INIT_SERV_DESC</code>.
+ * - The packet lengths specified in <code>#CR_FW_OUTCMP_INIT_KIND_DESC</code> are
+ *   greater than zero.
+ * .
+ * @return true if no errors are detected in the configuration data;
+ * false otherwise.
+ */
+CrFwBool_t CrFwAuxOutFactoryConfigCheck();
+
+/**
+ * Check the configuration of the InCommand part of the InFactory component.
+ * The following checks are performed on the configuration data in
+ * <code>CrFwInFactoryUserPar.h</code>:
+ * - CR_FW_INFACTORY_MAX_NOF_INCMD is greater than zero
+ * - CR_FW_INCMD_NKINDS is greater than zero
+ * - The service types are listed in increasing order in the service descriptor
+ *   initializer (<code>CR_FW_INCMD_INIT_KIND_DESC</code>)
+ * - The service sub-types within a type are listed in increasing order in the service
+ *   descriptor initializer (<code>CR_FW_INCMD_INIT_KIND_DESC</code>)
+ * - The discriminant values within a type/sub-type are listed in increasing order
+ *   in the service descriptor initializer (<code>CR_FW_INCMD_INIT_KIND_DESC</code>)
+ * - The values of the service types, sub-types and discriminant are lower than
+ * 	 <code>#CR_FW_MAX_SERV_TYPE</code>, <code>#CR_FW_MAX_SERV_SUBTYPE</code> and
+ * 	 <code>#CR_FW_MAX_DISCRIMINANT</code>.
+ * .
+ * @return true if no errors are detected in the configuration data;
+ * false otherwise.
+ */
+CrFwBool_t CrFwAuxInFactoryInCmdConfigCheck();
+
+/**
+ * Check the configuration of the InReport part of the InFactory component.
+ * The following checks are performed on the configuration data in
+ * <code>CrFwInFactoryUserPar.h</code>:
+ * - CR_FW_INFACTORY_MAX_NOF_INREP is greater than zero
+ * - CR_FW_INREP_NKINDS is greater than zero
+ * - The service types are listed in increasing order in the service descriptor
+ *   initializer (<code>CR_FW_INREP_INIT_KIND_DESC</code>)
+ * - The service sub-types within a type are listed in increasing order in the service
+ *   descriptor initializer (<code>CR_FW_INREP_INIT_KIND_DESC</code>)
+ * - The discriminant values within a type/sub-type are listed in increasing order
+ *   in the service descriptor initializer (<code>CR_FW_INREP_INIT_KIND_DESC</code>)
+ * - The values of the service types, sub-types and discriminant are lower than
+ * 	 <code>#CR_FW_MAX_SERV_TYPE</code>, <code>#CR_FW_MAX_SERV_SUBTYPE</code> and
+ * 	 <code>#CR_FW_MAX_DISCRIMINANT</code>.
+ * .
+ * @return true if no errors are detected in the configuration data;
+ * false otherwise.
+ */
+CrFwBool_t CrFwAuxInFactoryInRepConfigCheck();
+
+#endif /* CRFW_AUX_H_ */
