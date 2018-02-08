8482c0c719c3d51614e9a245e3400705135c66b4
Pasetti <pasetti@pnp-software.com>
2018-02-08 13:44:53 +0100

    Modified licence statement (LGPL to MPL)

--- src/OutManager/CrFwOutManager.h
+++ src/OutManager/CrFwOutManager.h
@@ -57,20 +57,11 @@
  * @author Alessandro Pasetti <pasetti@pnp-software.com>
  * @copyright P&P Software GmbH, 2013, All Rights Reserved
  *
- * This file is part of CORDET Framework.
+ * This file is part of the CORDET Framework.
  *
- * CORDET Framework is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * CORDET Framework is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with CORDET Framework.  If not, see <http://www.gnu.org/licenses/>.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  * For information on alternative licensing, please contact P&P Software GmbH.
  */
